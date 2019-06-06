1270b12e441c602b407ca7c80d916c745257908f
Nesrovaf <a21663832@gmail.com>
2019-06-06 14:59:19 +0800

    modified:   website/main.css 	modified:   website/publication.html 	modified:   website/secondary.css 	modified:   website/secondary.js

--- website/secondary.js
+++ website/secondary.js
@@ -2,20 +2,20 @@ var test = 0;
 
 function openside(){
     if(test%2==0){
-        document.getElementById("slide").style.width = "50vw";
+        document.getElementById("slide_m").style.width = "50vw";
         document.getElementById("slide_btn").style.marginLeft = "50vw";
         document.getElementById("slide_btn").style.width = "50vw";
-        var art = document.getElementsByClassName('article');
+        var art = document.getElementsByClassName('article_m');
         for(var i=0;i<art.length;i++){
             art[i].style.width="50vw";
             art[i].style.marginLeft="50vw";
         }
     }
     else{
-        document.getElementById("slide").style.width = "0vw";
+        document.getElementById("slide_m").style.width = "0vw";
         document.getElementById("slide_btn").style.marginLeft = "0vw";
         document.getElementById("slide_btn").style.width = "100vw";
-        var art = document.getElementsByClassName('article');
+        var art = document.getElementsByClassName('article_m');
         for(var i=0;i<art.length;i++){
             art[i].style.width="100vw";
             art[i].style.marginLeft="0vw";
