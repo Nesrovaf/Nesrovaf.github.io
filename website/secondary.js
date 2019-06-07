8051d50fa6edd32626702d59fc57bca49828e271
Nesrovaf <a21663832@gmail.com>
2019-06-07 22:51:25 +0800

    modified:   website/award.html 	modified:   website/contact.html 	modified:   website/experience.html 	modified:   website/main.css 	modified:   website/main.html 	modified:   website/project.html 	modified:   website/publication.html 	modified:   website/secondary.css 	modified:   website/secondary.js 	modified:   website/services.html

--- website/secondary.js
+++ website/secondary.js
@@ -1,25 +1,43 @@
 var test = 0;
-
-function openside(){
-    if(test%2==0){
+var cir = true;
+function openside() {
+    if (test % 2 == 0) {
         document.getElementById("slide_m").style.width = "50vw";
         document.getElementById("slide_btn").style.marginLeft = "50vw";
         document.getElementById("slide_btn").style.width = "50vw";
         var art = document.getElementsByClassName('article_m');
-        for(var i=0;i<art.length;i++){
-            art[i].style.width="50vw";
-            art[i].style.marginLeft="50vw";
+        for (var i = 0; i < art.length; i++) {
+            art[i].style.width = "50vw";
+            art[i].style.marginLeft = "50vw";
         }
     }
-    else{
+    else {
         document.getElementById("slide_m").style.width = "0vw";
         document.getElementById("slide_btn").style.marginLeft = "0vw";
         document.getElementById("slide_btn").style.width = "100vw";
         var art = document.getElementsByClassName('article_m');
-        for(var i=0;i<art.length;i++){
-            art[i].style.width="100vw";
-            art[i].style.marginLeft="0vw";
+        for (var i = 0; i < art.length; i++) {
+            art[i].style.width = "100vw";
+            art[i].style.marginLeft = "0vw";
         }
     }
-    test = (test+1)%2
+    test = (test + 1) % 2
+}
+function change_baclground() {
+    if (cir) {
+        let node;
+        document.getElementsByClassName("circle")[0].style.left = "27px";
+        document.getElementsByClassName("mid_layer")[0].style.width = "50px";
+        document.body.style.backgroundColor = "#FFFFFF";
+        document.getElementById("slide").style.backgroundColor = "#222222";
+        document.getElementsByClassName("circle")[0].style.backgroundColor = "#FFFFFF"
+    }
+    else {
+        document.getElementsByClassName("circle")[0].style.left = "2.5px";
+        document.getElementsByClassName("mid_layer")[0].style.width = "0px";
+        document.body.style.backgroundColor = "#222222";
+        document.getElementById("slide").style.backgroundColor = "#FFFFFF";
+        document.getElementsByClassName("circle")[0].style.backgroundColor = "#333333"
+    }
+    cir = !cir;
 }
